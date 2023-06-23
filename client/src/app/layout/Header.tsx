d2b85fa6e3d36062b431bcbd902ea9a58714343f
CezarVlaescu <108024461+CezarVlaescu@users.noreply.github.com>
2023-06-23 13:30:22 +0300

    update 5

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 interface Props {
@@ -19,33 +19,48 @@ const rightLinks =[
     {title : 'register', path: '/register'},
 ]
 
+const navStyles = {
+    color: 'inherit', 
+    typography: 'h6', 
+    '&:hover' : {
+        color : 'grey.500'
+    },
+    '&.active' : {
+        color: 'text.secondary'
+    }      
+}
+
 export default function Header({darkMode, handleThemeChanges} : Props){
     return (
         <AppBar position='static'sx={{mb: 4}}>            
-            <Toolbar>
-                <Typography variant="h6" component={NavLink} to='/' sx={{color : 'inherit', textDecoration : 'none'}}>
-                    Re-store
-                </Typography>
-                <Switch checked={darkMode} onChange={handleThemeChanges} value="checkedA" inputProps={{ 'aria-label': 'position' }} color='secondary' />
+            <Toolbar sx={{display: "flex", justifyContent : 'space-between', alignItems: 'center'}}>
+                <Box display="flex" alignItems="center">
+                    <Typography variant="h6" component={NavLink} to='/' sx={navStyles}>
+                        Re-store
+                    </Typography>
+                    <Switch checked={darkMode} onChange={handleThemeChanges} value="checkedA" inputProps={{ 'aria-label': 'position' }} color='secondary' />
+                </Box>              
                 <List sx={{display: 'flex'}}>
                     {midLinks.map(({title, path}) => ((
-                        <ListItem component={NavLink} to={path} key={path} sx={{color: 'inherit', typography: 'h6'}}>
+                        <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
                             {title.toUpperCase()}
                         </ListItem>
                     )))}
                 </List>
-                <IconButton size='large' sx={{color: 'inherit'}}>
-                    <Badge badgeContent={4} color='secondary'>
-                        <ShoppingCart />
-                    </Badge>
-                </IconButton>
-                <List sx={{display: 'flex'}}>
-                    {rightLinks.map(({title, path}) => ((
-                        <ListItem component={NavLink} to={path} key={path} sx={{color: 'inherit', typography: 'h6'}}>
-                            {title.toUpperCase()}
-                        </ListItem>
-                    )))}
-                </List>
+                <Box display="flex" alignItems="center">
+                    <IconButton size='large' sx={{ color: 'inherit' }}>
+                        <Badge badgeContent={4} color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => ((
+                            <ListItem component={NavLink} to={path} key={path} sx={{ color: 'inherit', typography: 'h6' }}>
+                                {title.toUpperCase()}
+                            </ListItem>
+                        )))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     )
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,9 +1,60 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product";
 
 export default function ProductDetails(){
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {id} = useParams<{id : string}>();
+    const [product, setProduct] = useState<Product | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5054/api/Products/${id}`)
+        .then(response => setProduct(response.data))
+        .catch(err => console.log(err))
+        .finally(()=>setLoading(false));
+    }, [id])
+
+    if(loading) return <h3>Loading...</h3>
+
     return (
-        <Typography variant="h2">
-            Product Details
-        </Typography>
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img src={product!.pictureUrl} alt={product!.name} style={{width: '100'}} />
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="h3">{product!.name}</Typography>
+                <Divider sx={{mb:2}}/>
+                <Typography variant="h4" color='secondary'>{(product!.price / 100).toFixed(2)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product!.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product!.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product!.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product!.brand}</TableCell>
+                            </TableRow><TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product!.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
     )
 }
